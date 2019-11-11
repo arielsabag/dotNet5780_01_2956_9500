7c2cc5f5274f5ca284482a7128c401739de9da1f
arielsabag <arielsabagsabag@gmail.com>
2019-11-11 15:16:40 +0200

    In the middle of part 2

--- dotNet5780_01_2956_9500/part2/Program.cs
+++ dotNet5780_01_2956_9500/part2/Program.cs
@@ -8,8 +8,50 @@ namespace part2
 {
     class Program
     {
+        enum choice { requirment, schedule, occupied, exit };
         static void Main(string[] args)
         {
+            
+            bool exit = false;
+            bool[,] Calendar = new bool[12, 31];
+            for (int i = 0; i < Calendar.Length/31; i++)
+            {
+                for (int j = 0; j < Calendar.Length/12; j++)
+                {
+                    //Calendar[i, j] = false;
+                }
+            }
+
+            do
+            {
+                Console.WriteLine("choose 1 for customers requirment for hospitality");
+                Console.WriteLine("choose 2 to view the annual list of all accomadation periods");
+                Console.WriteLine("choose 3 to display the total number of days occupied per year and the percentage of annual occupancy");
+                Console.WriteLine("choose 4 to exit");
+                int decision= Int32.Parse(Console.ReadLine());
+                switch (decision)
+                {
+                    case (int)choice.requirment:
+                        int month, day, length;
+                        Console.WriteLine("enter day");
+                        Console.WriteLine("enter month");
+                        Console.WriteLine("enter length");
+                        break;
+
+                    case (int)choice.schedule:
+                        break;
+
+                    case (int)choice.occupied:
+                        break;
+
+                    case (int)choice.exit:
+                        exit = true;
+                        break;
+
+                }
+            } while (!exit);
+
+            Console.ReadKey();
         }
     }
 }
